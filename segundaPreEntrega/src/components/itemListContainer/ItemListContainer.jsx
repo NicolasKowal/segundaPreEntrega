c07d9748f0754bfbd73394d1fd9a1523a6445f84
NicolasKowal <nicolas.kowal95@gmail.com>
2024-07-20 18:02:06 -0300

    .

--- segundaPreEntrega/src/components/itemListContainer/ItemListContainer.jsx
+++ segundaPreEntrega/src/components/itemListContainer/ItemListContainer.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import ItemList from "../itemList/ItemList";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./itemlist.css";
+import { Productos } from "../Productos";
 
 function ItemListContainer() {
-	return <></>;
+	return (
+		<div className="grid-container">
+			{Productos.map((producto) => (
+				<div key={producto.id}>
+					<ItemList
+						nombre={producto.nombre}
+						descripcion={producto.descripcion}
+						precio={producto.precio}
+						imagen={producto.imagen}
+						categoria={producto.categoria}
+					/>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default ItemListContainer;
