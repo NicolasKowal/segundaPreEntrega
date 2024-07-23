a92027abc6d00738e63ea1aa43fef44ffaa236d4
NicolasKowal <nicolas.kowal95@gmail.com>
2024-07-22 22:52:49 -0300

    .

--- segundaPreEntrega/src/components/AcercaDe.jsx
+++ segundaPreEntrega/src/components/AcercaDe.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function AcercaDe() {
-	return <div>AcercaDe</div>;
+	return (
+		<div>
+			<form action="POST" className="d-flex flex-column w-50 mx-auto">
+				<legend>nombre</legend>
+				<input type="first_name" name="first_name" id="" />
+				<legend>apellido</legend>
+				<input type="last_name" name="last_name" id="" />
+				<legend>telefono</legend>
+				<input type="phone" name="phone" id="" />
+				<legend>mail</legend>
+				<input type="email" name="email" id="" />
+				<legend>mensaje</legend>
+				<textarea name="text" id=""></textarea>
+				<button type="submit">go</button>
+				<button type="reset">reset</button>
+			</form>
+		</div>
+	);
 }
 
 export default AcercaDe;
