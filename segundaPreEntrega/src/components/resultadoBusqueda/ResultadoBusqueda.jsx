8c2ef1a1a9d5aca717eea2a60f50552bc4513b03
NicolasKowal <nicolas.kowal95@gmail.com>
2024-07-26 12:30:32 -0300

    buusqueda error

--- segundaPreEntrega/src/components/resultadoBusqueda/ResultadoBusqueda.jsx
+++ segundaPreEntrega/src/components/resultadoBusqueda/ResultadoBusqueda.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import { Productos } from "../Productos";
 import { useParams } from "react-router-dom";
+import ItemList from "../itemList/ItemList";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function ResultadoBusqueda() {
 	const { valor } = useParams();
-	console.log(valor);
-	return <>{valor}</>;
+	let productosFiltrados = Productos.filter((e) => e.tags.includes(valor));
+	if (productosFiltrados.length === 0)
+		return (
+			<img
+				className="typeOfError"
+				src="https://http.dog/498.jpg"
+				alt="not-found"
+			/>
+		);
+	return (
+		<div className="grid-container">
+			{productosFiltrados.map((producto) => (
+				<div key={producto.id}>
+					<ItemList
+						nombre={producto.nombre}
+						descripcion={producto.descripcion}
+						precio={producto.precio}
+						imagen={producto.imagen}
+						categoria={producto.categoria}
+						stock={producto.stock}
+						id={producto.id}
+					/>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default ResultadoBusqueda;
